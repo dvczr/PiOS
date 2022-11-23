ca4f586f38d1547bc78de4b8d859c18a2c61579f
dvczr <90769079+dvczr@users.noreply.github.com>
2022-11-23 12:08:32 +0100

    Initial Commit

---
+++ PiOS/PiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  PiOSApp.swift
+//  PiOS
+//
+//  Created by David Zirath on 2022-11-23.
+//
+
+import SwiftUI
+
+@main
+struct PiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
